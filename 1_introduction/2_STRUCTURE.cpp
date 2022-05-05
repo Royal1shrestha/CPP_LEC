b8d69750c6832894b186585287938108302b7717
Royal1shrestha <sthapraex@gmail.com>
2022-05-05 13:56:00 +0545

    Structure

---
+++ 1_introduction/2_STRUCTURE.cpp
@@ -0,0 +1,23 @@
+// Q. WAP in C++ to read the record the student's name,address,roll no and display them using structure.
+#include<iostream>
+
+using namespace std;
+struct student
+    {
+        char name[50],address[50];
+        int roll;
+    }st;
+
+int main()
+{
+        cout<<"Enter the name of the student: ";
+        cin>>st.name;
+        cout<<"Enter the address of the student: ";
+        cin>>st.address;
+        cout<<"Enter the roll no of the student: ";
+        cin>>st.roll;
+        cout<<"Name: "<<st.name;
+        cout<<"\n Address: "<<st.address;
+        cout<<"\n Roll no: "<<st.roll;
+        return 0;
+}
