d414b5b8cb53cac1273082c78558041d9da12e9c
Royal1shrestha <sthapraex@gmail.com>
2022-05-19 14:01:05 +0545

    Function overloadding / Default argument

---
+++ 1_introduction/4_ARRAY.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[20] = {1,2,3,4};
+    cout<<"Array of 0 index is :"<<a[0];
+    cout<<"\nArray of 1 index is :"<<a[1];
+    cout<<"\nArray of 2 index is :"<<a[2];
+    cout<<"\nArray of 3 index is :"<<a[3];
+}
