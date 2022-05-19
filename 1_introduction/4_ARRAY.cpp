b6909fc7de8350330894a1c905cc45a0a1c7c64e
Royal1shrestha <sthapraex@gmail.com>
2022-05-19 14:09:45 +0545

    Array

--- 1_introduction/4_ARRAY.cpp
+++ 1_introduction/4_ARRAY.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main()
 {
     int a[20] = {1,2,3,4};
-    cout<<"Array of 0 index is :"<<a[0];
-    cout<<"\nArray of 1 index is :"<<a[1];
-    cout<<"\nArray of 2 index is :"<<a[2];
-    cout<<"\nArray of 3 index is :"<<a[3];
+    cout<<"Array of 0 index is :"<<a[0]<<endl;
+    cout<<"Array of 1 index is :"<<a[1]<<endl;
+    cout<<"Array of 2 index is :"<<a[2]<<endl;
+    cout<<"Array of 3 index is :"<<a[3];
 }
