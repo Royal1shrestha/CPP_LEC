15fa81a76e5821b602ece1578cf6b63a823bbcee
Royal1shrestha <sthapraex@gmail.com>
2022-06-17 19:36:06 +0545

    Operator

--- Chapter_6_inheritance/rough.cpp
+++ Chapter_6_inheritance/rough.cpp
@@ -7,15 +7,27 @@ class A
     int x;
     int y;
 
-    void display()
+    void getdata(int a,int b)
     {
-        cout<<"This is display";
+        x=a;
+        y=b;
     }
 
+    A operator +(A &a1)
+    {
+        A a;
+        a.x=a1.x+x;
+        a.y=a1.y+y;
+        return a;
+    }
 };
 
 int main()
 {
     A a1,a2,a3;
-    // a3=a1+a2;
+    a1.getdata(2,3);
+    a2.getdata(5,6);
+    a3=a1+a2;
+    cout<<"Value of sum of x : "<<a3.x<<endl;
+    cout<<"Value of sum of y : "<<a3.y<<endl;
 }
