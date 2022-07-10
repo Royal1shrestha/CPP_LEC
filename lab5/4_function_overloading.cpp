e2874e3f59e5f72157d86111759767f97286de7c
Royal1shrestha <sthapraex@gmail.com>
2022-07-10 12:50:07 +0545

    Areabcalculation

---
+++ lab5/4_function_overloading.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+class Area
+{
+public:
+    void calculateArea(float length)
+    {
+        cout << "The area of square is " << length * length << endl;
+    }
+    void calculateArea(float length, float breadth)
+    {
+        cout << "The area of rectangle is " << length * breadth << endl;
+    }
+
+    void calculateArea(double a, double b, double c)
+    {
+        double s, area;
+        s = (a + b + c) / 2;
+        area = sqrt(s * (s - a) * (s - b) * (s - c));
+        cout << "The area of triangle is " << area << endl;
+    }
+};
+int main()
+{
+    Area a;
+    a.calculateArea(1.1, 2.2, 2.2);
+    a.calculateArea(5.5, 6.6);
+    a.calculateArea(9.9);
+}
