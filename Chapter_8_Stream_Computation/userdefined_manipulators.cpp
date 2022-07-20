c3271a65fa5c1dd2ef53a45fd1a0201e95612a47
Royal1shrestha <sthapraex@gmail.com>
2022-07-20 17:41:03 +0545

    Manipulators

---
+++ Chapter_8_Stream_Computation/userdefined_manipulators.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+ostream &tb(ostream &os)
+{
+    os<<'\t'<<flush;
+    return os;
+}
+
+int main()
+{
+    int a = 1, b = 2, c = 3, d = 4;
+
+    cout << a << tb << b << tb << tb << d << endl;
+
+    return 0;
+}
